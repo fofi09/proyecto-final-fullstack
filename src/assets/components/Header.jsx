60702348691485772fdd2cd1f31a9cfc20682d89
sofia aguirre <aguirreanasofia07@gmail.com>
2025-09-11 20:34:20 -0300

    fin2

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const Header = () => {
     return (
         <nav className="flex items-center gap-6 md:gap-36 text-sm font-medium text-gray-700">
-            <a href="#" className="hover:text-emerald-700 after:content-['•'] after:mx-2 last:after:content-none">
+            <a href="#" className="hover:text-emerald-700">
                 About Us
             </a>
-            <a href="#" className="hover:text-emerald-700 after:content-['•'] after:mx-2 last:after:content-none">
+            <a href="#" className="hover:text-emerald-700">
                 Campaign
             </a>
-            <a href="#" className="hover:text-emerald-700 last:after:content-none">
+            <a href="#" className="hover:text-emerald-700">
                 Contact Us
             </a>
         </nav>
